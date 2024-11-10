de29ff018e21973aa6de2c5185928b145583a11f
Nandhu Krishna <nandhukrishna393@gmail.com>
2024-11-10 22:40:28 +0530

    connected postgres

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
-function sayHello(name : string){
- console.log('Hello ', name);
-}
+import express from 'express';
 
-sayHello('Nandhu')
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/' , (req,res)=> {
+    res.send("Hello World")
+})
+
+app.listen(port , ()=> {
+    console.log(`Server is running on port : ${port}`);
+})
---
+++ src/models/student_model.ts
@@ -0,0 +1,6 @@
+export type Students = {
+    id?: number;
+    name: string;
+    age: number;
+    email: string;
+}
